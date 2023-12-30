b69fbbb3bf2f75502d392fdeac5543c93d68ce9a
Abdoulanziz <abdoulanzizally@outlook.com>
2023-12-30 22:45:27 +0300

    Update README.md.

--- seeders/20231007203703-demo-user.js
+++ seeders/20231007203703-demo-user.js
@@ -7,14 +7,6 @@ module.exports = {
     return queryInterface.bulkInsert('users', [
       {
         user_uuid: uuidv4(),
-        username: 'admin',
-        password: '$2b$10$BfxM7ko7ILI8XPfMA95WL.tTWsfuzV5L6cKfwc8K3RazQVCuywrAy',
-        role_id: 2,
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        user_uuid: uuidv4(),
         username: 'felix',
         password: '$2b$10$BfxM7ko7ILI8XPfMA95WL.tTWsfuzV5L6cKfwc8K3RazQVCuywrAy',
         role_id: 3,
