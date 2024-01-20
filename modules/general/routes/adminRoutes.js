8cef6cdcac876380fe60eccbfa56246e33a37f55
Abdoulanziz <abdoulanzizally@outlook.com>
2024-01-20 16:56:16 +0300

    Update auth route for admin on general module.

--- modules/general/routes/adminRoutes.js
+++ modules/general/routes/adminRoutes.js
@@ -5,11 +5,11 @@ const { requireLogin, checkUserRoleId } = require("../middlewares/auth");
 const { renderDashboard, renderAccounts, renderActivities, renderInventory, renderReports, renderSettings } = require("../controllers/adminController");
 
 // TODO: moveRoleId to .env
-router.get("/dashboard", requireLogin, checkUserRoleId(1), renderDashboard);
-router.get("/accounts", requireLogin, checkUserRoleId(1), renderAccounts);
-router.get("/activities", requireLogin, checkUserRoleId(1), renderActivities);
-router.get("/inventory", requireLogin, checkUserRoleId(1), renderInventory);
-router.get("/reports", requireLogin, checkUserRoleId(1), renderReports);
-router.get("/settings", requireLogin, checkUserRoleId(1), renderSettings);
+router.get("/dashboard", requireLogin, checkUserRoleId(2), renderDashboard);
+router.get("/accounts", requireLogin, checkUserRoleId(2), renderAccounts);
+router.get("/activities", requireLogin, checkUserRoleId(2), renderActivities);
+router.get("/inventory", requireLogin, checkUserRoleId(2), renderInventory);
+router.get("/reports", requireLogin, checkUserRoleId(2), renderReports);
+router.get("/settings", requireLogin, checkUserRoleId(2), renderSettings);
 
 module.exports = router;
